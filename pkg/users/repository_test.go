4cebb46458d1fcbf87d243f4cec21f7f90a6a833
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2023-07-01 17:19:44 +0200

    feat: More tests

--- pkg/users/repository_test.go
+++ pkg/users/repository_test.go
@@ -98,6 +98,7 @@ func (suite *UsersRepositoryTestSuite) TestGetUser_WrongId() {
 	repo.Create(defaultTestUser)
 
 	wrongId := uuid.New()
+	assert.NotEqual(defaultTestUser.Id, wrongId)
 	_, err := repo.Get(wrongId)
 	assert.True(errors.IsErrorWithCode(err, errors.ErrNoSuchUser))
 }
@@ -132,4 +133,120 @@ func (suite *UsersRepositoryTestSuite) TestGetAll() {
 	assert.Nil(err)
 	assert.Equal(1, len(ids))
 	assert.Equal(defaultTestUser.Id, ids[0])
+}
+
+func (suite *UsersRepositoryTestSuite) TestPatchUser_Empty() {
+	assert := assert.New(suite.T())
+
+	repo := suite.createRepo()
+	repo.Create(defaultTestUser)
+
+	patch := User{}
+	patched, err := repo.Patch(defaultTestUser.Id, patch)
+	assert.Nil(err)
+
+	assert.Equal(defaultTestUser.Id, patched.Id)
+	assert.Equal(defaultTestUser.Mail, patched.Mail)
+	assert.Equal(defaultTestUser.Name, patched.Name)
+	assert.Equal(defaultTestUser.Password, patched.Password)
+}
+
+func (suite *UsersRepositoryTestSuite) TestPatchUser_Mail() {
+	assert := assert.New(suite.T())
+
+	repo := suite.createRepo()
+	repo.Create(defaultTestUser)
+
+	patch := User{Mail: "some-other@mail"}
+	patched, err := repo.Patch(defaultTestUser.Id, patch)
+	assert.Nil(err)
+
+	assert.Equal(defaultTestUser.Id, patched.Id)
+	assert.Equal(patch.Mail, patched.Mail)
+	assert.Equal(defaultTestUser.Name, patched.Name)
+	assert.Equal(defaultTestUser.Password, patched.Password)
+}
+
+func (suite *UsersRepositoryTestSuite) TestPatchUser_Name() {
+	assert := assert.New(suite.T())
+
+	repo := suite.createRepo()
+	repo.Create(defaultTestUser)
+
+	patch := User{Name: "someOtherName"}
+	patched, err := repo.Patch(defaultTestUser.Id, patch)
+	assert.Nil(err)
+
+	assert.Equal(defaultTestUser.Id, patched.Id)
+	assert.Equal(defaultTestUser.Mail, patched.Mail)
+	assert.Equal(patch.Name, patched.Name)
+	assert.Equal(defaultTestUser.Password, patched.Password)
+}
+
+func (suite *UsersRepositoryTestSuite) TestPatchUser_Password() {
+	assert := assert.New(suite.T())
+
+	repo := suite.createRepo()
+	repo.Create(defaultTestUser)
+
+	patch := User{Password: "somePassword"}
+	patched, err := repo.Patch(defaultTestUser.Id, patch)
+	assert.Nil(err)
+
+	assert.Equal(defaultTestUser.Id, patched.Id)
+	assert.Equal(defaultTestUser.Mail, patched.Mail)
+	assert.Equal(defaultTestUser.Name, patched.Name)
+	assert.Equal(patch.Password, patched.Password)
+}
+
+func (suite *UsersRepositoryTestSuite) TestPatchUser_IdIsNotPatched() {
+	assert := assert.New(suite.T())
+
+	repo := suite.createRepo()
+	repo.Create(defaultTestUser)
+
+	patch := User{Id: uuid.New()}
+	assert.NotEqual(defaultTestUser.Id, patch.Id)
+	patched, err := repo.Patch(defaultTestUser.Id, patch)
+	assert.Nil(err)
+
+	assert.Equal(defaultTestUser.Id, patched.Id)
+	assert.Equal(defaultTestUser.Mail, patched.Mail)
+	assert.Equal(defaultTestUser.Name, patched.Name)
+	assert.Equal(defaultTestUser.Password, patched.Password)
+}
+
+func (suite *UsersRepositoryTestSuite) TestDeleteUser_NoUsers() {
+	assert := assert.New(suite.T())
+
+	repo := suite.createRepo()
+
+	someId := uuid.New()
+	err := repo.Delete(someId)
+	assert.True(errors.IsErrorWithCode(err, errors.ErrNoSuchUser))
+}
+
+func (suite *UsersRepositoryTestSuite) TestDeleteUser_WrongId() {
+	assert := assert.New(suite.T())
+
+	repo := suite.createRepo()
+	repo.Create(defaultTestUser)
+
+	wrongId := uuid.New()
+	assert.NotEqual(defaultTestUser.Id, wrongId)
+	err := repo.Delete(wrongId)
+	assert.True(errors.IsErrorWithCode(err, errors.ErrNoSuchUser))
+}
+
+func (suite *UsersRepositoryTestSuite) TestDeleteUser() {
+	assert := assert.New(suite.T())
+
+	repo := suite.createRepo()
+	repo.Create(defaultTestUser)
+
+	err := repo.Delete(defaultTestUser.Id)
+	assert.Nil(err)
+
+	_, err = repo.Get(defaultTestUser.Id)
+	assert.True(errors.IsErrorWithCode(err, errors.ErrNoSuchUser))
 }
