776c19c22ddaea54e39c42ed33b3103dec463a0d
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2023-07-08 23:27:33 +0200

    feat: Deleted patch method for now, too complicated

--- pkg/users/db_repository.go
+++ pkg/users/db_repository.go
@@ -86,10 +86,6 @@ func (repo *userDbRepo) Get(id uuid.UUID) (User, error) {
 	return scanner.user, nil
 }
 
-func (repo *userDbRepo) Patch(id uuid.UUID, patch User) (User, error) {
-	return User{}, errors.NewCode(errors.ErrNotImplemented)
-}
-
 func (repo *userDbRepo) Delete(id uuid.UUID) error {
 	qb := deleteQueryBuilderFunc()
 
